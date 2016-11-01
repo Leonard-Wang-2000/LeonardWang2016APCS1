09ab3d5d80934bab7fff2e2a2e4dac147c70ca02
LeonardWang2000 <18wangl64@millshighsch.org>
2016-11-01 09:28:05 -0700

    ArrayLabs3

--- ArrayLab3/src/Arrayslab3.java
+++ ArrayLab3/src/Arrayslab3.java
@@ -2,8 +2,26 @@
 public class Arrayslab3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	}
+	public static int[] sum(int [] arr1, int [] arr2){
+		int [] arr3 = new int[arr1.length];
+		for(int i = 0; i < arr3.length; i++){
+			arr3[i] = arr1[i] + arr2[i];
+		}
+		return arr3;
+	}
+	public static int[] append(int [] arr, int num){
+		
+	}
+	public static int[] remove(int[] arr, int idx){
+		
+	}
+	public static int sumEven(int [] arr){
+		
+	}
+	public static void rotateRight(int[] arr){
+		
 	}
 
 }
