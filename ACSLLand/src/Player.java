d410dbc5c4eb3f692492f5e87fb1f85dadd459ad
LeonardWang2000 <18wangl64@millshighsch.org>
2016-12-13 09:28:41 -0800

    kkulglgkgkjgk.jjk

---
+++ ACSLLand/src/Player.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Player {
+	private int position = 0;
+	public void setPosition(int position){
+		this.position = position;
+	}
+	public int getPosition(){
+		return this.position;
+	}
+}
