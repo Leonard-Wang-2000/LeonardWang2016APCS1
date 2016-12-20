935b7113b929ade2e33d1b5265161d17011b2606
Leonard Wang <18wangl64@millshighsch.org>
2016-12-19 23:11:22 -0800

    bhlkhhbj

--- ProgrammingExercisesAPCS1/src/testSomeStuff.java
+++ ProgrammingExercisesAPCS1/src/testSomeStuff.java
@@ -1,9 +1,20 @@
+import java.util.Arrays;
 
 public class testSomeStuff {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(-8%1);
-	}
-
+		int [] arr = {2, 4, 6, 8, 20};
+		int counter = 0;
+		for(int n:arr){
+			if(n %4== 0){
+				counter++;
+			}
+		}
+		System.out.println(counter);
+	
+//	for(int i = 0; i < arr.length; i++){
+//		arr[i] = arr[i] + 5;
+//	}
+//	System.out.println(Arrays.toString(arr));
+}
 }
